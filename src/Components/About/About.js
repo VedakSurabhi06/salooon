d8330580f1e4196792fd21e5a496c1f2d6414b7d
Surabhi Vedak <surabhi.vedak@gmail.com>
2022-06-06 00:56:14 +0530

    fifth

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,22 +14,17 @@ const About = () => {
           />
         </div>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
-          placeat earum totam distinctio cum eum sed? Animi inventore veniam
-          maiores, temporibus vitae ad sed dolore totam, a architecto incidunt
-          cupiditate.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
-          placeat earum totam distinctio cum eum sed? Animi inventore veniam
-          maiores, temporibus vitae ad sed dolore totam, a architecto incidunt
-          cupiditate.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
-          placeat earum totam distinctio cum eum sed? Animi inventore veniam
-          maiores, temporibus vitae ad sed dolore totam, a architecto incidunt
-          cupiditate.
+        At the ‘Love Your Hair’, we have confidence in excellence with a heart. We have made a salon that offers the most noteworthy quality hair benefits in a setting that is more advantageous for the earth, our visitors, and our staff.
+
+The ‘Love Your Hair’ is planned and fabricated essentially with reused materials: the floor is made of recovered wood from a nearby grain lift – protecting and displaying its common surface – and the vast majority of the installations and furniture are renovated or unique vintage pieces.
+
+We convey top-notch proficient hair items intended to guarantee our visitors put their best self forward, both in the salon and at home. The hair care items we convey have been cautiously picked put together both with respect to execution and eco-affectability.
+
+As a spotless air salon, we offer smelling salts-free shading administrations with almost no fragrance properties or substance spread.
+
+This more beneficial option in contrast to conventional shading rehearses is additionally ok for eager moms and people with hypersensitivities or fragrance sensitivities.
+
+Our structure and theory work are connected at the hip. We are focused on furnishing solid hair care with a low effect on the earth. Come see what you and your hair can improve the situation of the Earth.
         </p>
       </div>
     </div>
